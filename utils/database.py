51cd75c73ce84e2c6961a72931df3ad5e7fa2e85
Michael Herrmann <00mhe@web.de>
2025-02-13 11:07:09 +0100

    Neue Funftion hinzugefügt

---
+++ utils/database.py
@@ -0,0 +1,32 @@
+
+# Name feedback file
+FEED_BACK_FILE = 'feedback.txt'
+# Define regular expresion for date format DD.MM.YYYY
+DATE_PATTERN = '\d{2}.\d{2}.\d{4}'
+# Define search pattern
+SERACH_PATTERN_GERMAN  = 'exzellent'
+SERACH_PATTERN_ENGLISH = 'excellent'
+# Define feedback seperator
+FEED_BACK_SEPERATOR = '\n'
+# Define good rating
+EMOJI_GOOD_RATING = '\U0001f44d'
+# Define file date rate
+FILE_NAME_DATE_RATE = 'datums_vorkommen.json'
+# Define file good ratings 
+FILE_NAME_GOOD_FEEDBACKS = 'exzellente_feedbacks.json'
+# Define sample list of feedbacks
+ratings = list()
+ratings.append('Rezension aus Deutschland vom 07.12.2024. Klasse Fernseher. Er läuft und läuft, noch keine Probleme, auch nach diesen Jahren nicht. Der Service war exzellent. ')
+ratings.append('Rezension aus Deutschland vom 07.12.2024. Am besten ist Fernbedienung und Bild exzellent. ')
+ratings.append('Rezension aus Deutschland vom 07.12.2024. Der Fernseher läuft gut, auch die streaming Apps funktionieren einwandfrei. Dies ist ein exzellenter Fernseher. ')
+ratings.append('Rezension aus Deutschland vom 09.12.2024. Ich bin sehr zufrieden mit dem TV. Für den Preis kann man nicht mehr erwarten. Dies ist ein exzellentes Produkt. ')
+ratings.append('Rezension aus Deutschland vom 09.12.2024. Leicht zu installieren, exzellente Bildqualität. ')
+ratings.append('Rezension aus Deutschland vom 13.12.2024. Einfach zu bedienen exzellente Farbqualität. ')
+ratings.append('Rezension aus Deutschland vom 15.12.2024. Der Fernseher ist leider nach zweieinhalb Jahren defekt, schwarzer Bildschirm. ')
+ratings.append('Rezension aus England vom 18.12.2024. This is the Worst TV I have ever purchased. The LG App is a complete failure. ')
+ratings.append('Rezension aus England vom 18.12.2024. Setting up was super, simple Remote control is excellent. ')
+
+
+
+
+
