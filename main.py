f5273b604018a1e0e6b40b4fc98a6c5119621f2e
Michael Herrmann <00mhe@web.de>
2025-02-13 11:21:12 +0100

    Hauptdatei aktualisert

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from tkinter import Tk
 
 fenster = Tk()
 fenster.mainloop()
+
+
+# Neue Funktion hinzugekommen im feature branch
+def write_default_file(path_name:str,data:list):
+    try:
+      with open(path_name,'w', encoding='utf-8') as stream:
+          for item in data:
+              stream.write(item + FEED_BACK_SEPERATOR)
+          stream.close()
+    except Exception as e:
+        print('Can not write file: ',path_name,e)
